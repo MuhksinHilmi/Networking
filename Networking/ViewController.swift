ab3fa5ace6e4ba49f64232eef7af9b485d1d188b
hilmi <mfhilmi@binar.co.id>
2018-04-15 21:18:01 +0700

    first commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Muhksin Hilmi on 15/04/2018.
+//  Copyright © 2018 Shinkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
